49ea04fd8a45afcf92f42802855c36fe380807e7
Ian Lewis <IanMLewis@gmail.com>
2014-03-25 21:39:34 +0900

    Updated pyconjp settings

---
+++ pyconjp/settings/base.py
@@ -0,0 +1,11 @@
+from pycon.settings.base import *
+
+INSTALLED_APPS.append('restcms')
+INSTALLED_APPS.remove('symposion.cms')
+
+ROOT_URLCONF = 'pyconjp.urls'
+
+LANGUAGES = (
+    ('en', 'English'),
+    ('ja', 'Japanese'),
+)

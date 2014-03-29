54716d2b5bcac34d7bf4936756cb9bbda806ff87
Ian Lewis <IanMLewis@gmail.com>
2014-03-29 20:48:40 +0900

    Updated settings

--- pyconjp/settings/production.py
+++ pyconjp/settings/production.py
@@ -1,1 +1,22 @@
-from pycon.settings.production import *
+# Production settings
+from .server import *
+
+# TODO: 後で PyConJP 風に設定する
+
+# From address for production
+DEFAULT_FROM_EMAIL = "PyCon 2014 <no-reply@us.pycon.org>"
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
+
+LOGGING['filters']['static_fields']['fields']['environment'] = 'production'
+
+ALLOWED_HOSTS = [
+    'us.pycon.org',
+]
+
+LOGGING['handlers']['console']['filters'] = []
+LOGGING[''] = {
+    'handlers': ['console'],
+    'level': 'DEBUG',
+}
+import logging
+logging.basicConfig(level=logging.DEBUG)

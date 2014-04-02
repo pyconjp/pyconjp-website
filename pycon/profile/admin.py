bfb31a8b4aa04bad647e6f3028379e84a583ef93
MURAOKA Yusuke <yusuke@jbking.org>
2014-04-02 16:24:18 +0900

    introduce PyConJP profile for first_name_ja and last_name_ja by enable original profile app.

--- pycon/profile/admin.py
+++ pycon/profile/admin.py
@@ -5,10 +5,12 @@ from .models import Profile
 
 admin.site.register(
     Profile,
+    exclude=['phone'],
     list_display=[
         'user',
         'first_name',
+        'first_name_ja',
         'last_name',
-        'phone'
-    ]
+        'last_name_ja',
+    ],
 )

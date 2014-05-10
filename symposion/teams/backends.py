3fa11c49485c9745c697fc4bfd62b414a1a8b8d5
Ian Lewis <IanMLewis@gmail.com>
2014-05-10 16:48:37 +0900

    Updated the team permissions backend

--- symposion/teams/backends.py
+++ symposion/teams/backends.py
@@ -8,7 +8,7 @@ class TeamPermissionsBackend(object):
     def authenticate(self, username=None, password=None):
         return None
 
-    def get_team_permissions(self, user_obj, obj=None):
+    def get_all_permissions(self, user_obj, obj=None):
         """
         Returns a set of permission strings that this user has through his/her
         team memberships and managerships.
@@ -41,4 +41,15 @@ class TeamPermissionsBackend(object):
     def has_perm(self, user_obj, perm, obj=None):
         if not user_obj.is_active:
             return False
-        return perm in self.get_team_permissions(user_obj, obj)
+        return perm in self.get_all_permissions(user_obj, obj)
+
+    def has_module_perms(self, user_obj, app_label):
+        """
+        Returns True if user_obj has any permissions in the given app_label.
+        """
+        if not user_obj.is_active:
+            return False
+        for perm in self.get_all_permissions(user_obj):
+            if perm[:perm.index('.')] == app_label:
+                return True
+        return False

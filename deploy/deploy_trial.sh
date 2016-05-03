331a8e31c0f050d65d0f25104c8c5648d189aed4
rhoboro <rhoboro@gmail.com>
2016-05-03 17:52:52 +0900

    実行ユーザーをpyconjpに変更

--- deploy/deploy_trial.sh
+++ deploy/deploy_trial.sh
@@ -4,5 +4,5 @@ set -ex
 # please remove this deploy_trial.sh file after merging SAR-553-circleci branch
 
 ssh deploy@pycon.jp uptime
-ssh deploy@pycon.jp sh /opt/workspace/deploy-scripts/trial_update.sh
+ssh deploy@pycon.jp sh -u pyconjp /opt/workspace/deploy-scripts/trial_update.sh
 

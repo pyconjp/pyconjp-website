67015b57338d4bedd7f13ecbc28aa98519820e59
rhoboro <rhoboro@gmail.com>
2016-05-03 15:52:30 +0900

    CircleCIから叩くスクリプトを本番用に変更

--- deploy/deploy_staging.sh
+++ deploy/deploy_staging.sh
@@ -3,5 +3,5 @@ set -ex
 
 ssh deploy@pycon.jp uptime
 # please replace for deployment commands
-ssh deploy@pycon.jp sh -c 'date > /tmp/deploy-for-staging'
+ssh deploy@pycon.jp sh /opt/workspace/deploy-scripts/update.sh staging 2016
 

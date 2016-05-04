6f9572de26aaf8b7bd0938f44c0cf4608604184f
rhoboro <rhoboro@gmail.com>
2016-05-04 11:31:29 +0900

    trialでステージングをアップデート

--- deploy/deploy_trial.sh
+++ deploy/deploy_trial.sh
@@ -4,5 +4,5 @@ set -ex
 # please remove this deploy_trial.sh file after merging SAR-553-circleci branch
 
 ssh deploy@pycon.jp uptime
-ssh deploy@pycon.jp sudo -u pyconjp /opt/workspace/deploy-scripts/trial_update.sh
+ssh deploy@pycon.jp sudo -u pyconjp /opt/workspace/deploy-scripts/update.sh staging 2016
 

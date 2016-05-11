4306e987beba6fb4f5673bf6df74f1b9943984f0
shimizukawa <shimizukawa@gmail.com>
2016-05-11 21:08:00 +0900

    SAR-566 ブランチのための検証コードを削除

--- deploy/deploy_trial.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -ex
-
-# please remove this deploy_trial.sh file after merging SAR-553-circleci branch
-
-ssh deploy@pycon.jp uptime
-ssh deploy@pycon.jp sudo -u pyconjp /opt/workspace/deploy-scripts/update.sh staging 2016
-

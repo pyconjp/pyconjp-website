e64f4ae083c6435d509e12d846332ce66b4464f4
shimizukawa <shimizukawa@gmail.com>
2016-08-30 08:19:43 +0900

    refs #SAR-486 speakers list distinct & filter NULL

--- pyconjp/speaker/views.py
+++ pyconjp/speaker/views.py
@@ -9,6 +9,8 @@ from symposion.speakers.models import Speaker
 def speaker_list(request):
     speakers = Speaker.objects.filter().order_by('name')
     speakers = speakers.exclude(Q(presentations__isnull=True) & Q(copresentations__isnull=True))
+    speakers = speakers.exclude(user__isnull=True)
+    speakers = speakers.distinct()
 
     ctx = {
         "speakers": speakers,

311279376863710384c85dc3860e97abd4f10e47
shimizukawa <shimizukawa@gmail.com>
2016-10-01 14:41:23 +0900

    circleci で実行するスクリプトの実装をcircle.yml外へ移動

---
+++ scripts/circleci-dependencies.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -ex
+
+if [[ -e ~/docker/wheel.tar ]]; then docker load -i ~/docker/wheel.tar; fi
+docker info
+docker build -f Dockerfile.wheel -t pyconjp/wheelhouse:2016 .
+mkdir -p ~/docker; docker save pyconjp/wheelhouse:2016 > ~/docker/wheel.tar
+

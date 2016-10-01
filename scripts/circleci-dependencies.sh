58b3207efe8e051047a291d5f1d50b4ed83ba38b
shimizukawa <shimizukawa@gmail.com>
2016-10-01 14:48:21 +0900

    fix previous commit

--- scripts/circleci-dependencies.sh
+++ scripts/circleci-dependencies.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-if [[ -e ~/docker/wheel.tar ]]; then docker load -i ~/docker/wheel.tar; fi
+if [ -e ~/docker/wheel.tar ]; then docker load -i ~/docker/wheel.tar; fi
 docker info
 docker build -f Dockerfile.wheel -t pyconjp/wheelhouse:2016 .
 mkdir -p ~/docker; docker save pyconjp/wheelhouse:2016 > ~/docker/wheel.tar
